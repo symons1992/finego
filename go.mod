02f1b2ea6a11f0b637960dc16938c54518c379e8
symons1992 <romanticbao@gmail.com>
2021-03-10 20:52:47 +0800

    Update code

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module finego
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	gopkg.in/yaml.v2 v2.2.8
+)
--- main.go
+++ main.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"finego/handler"
+	"fmt"
+
+	"io/ioutil"
 
 	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v2"
 )
 
+type Config struct {
+	HttpPort string `yaml:"http_port"`
+}
 
 func main() {
 	r := gin.Default()
+
+	data, err := ioutil.ReadFile("conf/dev.yaml")
+	if err != nil {
+		panic(err)
+	}
+	var config Config
+	err = yaml.Unmarshal([]byte(data), &config)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("config is %+v\n", config)
+
 	r.GET("/ping", handler.HelloWorld)
-	r.Run()
+	r.Run(config.HttpPort)
 }
